6f13494ce91c826966636d65a7bf9174aefff047
corentinghestem <61146780+corentinghestem@users.noreply.github.com>
2020-02-28 14:59:15 +0100

    Update embed.js

--- Commandes/embed.js
+++ Commandes/embed.js
@@ -1,14 +1,12 @@
 const Discord = require('discord.js');
 
 module.exports.run = async(bot, message, args) =>{
+    message.delete();
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You need permissions btw")
-    
     let color = args[0]
     if(!color) return message.channel.send("Don't forget to choose a color !")
-
     let messageEmbed = args.slice(1).join(" ");
     if(!messageEmbed) return message.channel.send("You have to enter a message for your embed !")
-
     let botEmbed = new Discord.RichEmbed()
        .setColor(color)
        .setDescription(messageEmbed)
@@ -16,7 +14,6 @@ module.exports.run = async(bot, message, args) =>{
        .setTimestamp()
     message.channel.send(botEmbed);
 }
-
 module.exports.help = {
     name: "embed"
 }
