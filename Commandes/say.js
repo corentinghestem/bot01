d5c6367c3662473a1583b8ae2accd4f9fc692781
corentinghestem <61146780+corentinghestem@users.noreply.github.com>
2020-03-01 12:26:36 +0100

    Add files via upload

--- Commandes/say.js
+++ Commandes/say.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js');
 
 module.exports.run = async(bot, message, args) => {
+    message.delete();
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**You need permissions btw**").then(m => m.delete(5000))
+    if(!args[0]) return message.channel.send(`**You have to tell something ${message.author.username} !**`).then(m => m.delete(5000));
     let botMessage = args.join(" ");
-    message.delete()
     message.channel.send(botMessage);
     console.log(`> Commande réalisée par ${message.author.username} :
     -say ${args} `);
