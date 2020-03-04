1c46fd4548358018d97177daae5c082dc17896f3
corentinghestem <61146780+corentinghestem@users.noreply.github.com>
2020-03-04 12:38:35 +0100

    Add files via upload

--- Events/ready.js
+++ Events/ready.js
@@ -1,9 +1,13 @@
 module.exports = (bot) => {
-    bot.user.setPresence({
-        game: {
-            name: `a!help | ${bot.users.size} members`
-        }
-    })
+    const botStatut= [
+        "Use a!help",
+        `${bot.guilds.size} servers | ${bot.users.size} members`
+    ];
+    setInterval(function() {
+        var statutID = Math.floor(Math.random() * Math.floor(botStatut.length));
+        bot.user.setActivity(botStatut[statutID])
+    }, 5000)
+
     console.log(" ");
     console.log("Connecté en tant que " + bot.user.tag + " et présent sur " + bot.guilds.size + " serveurs !");
 };
