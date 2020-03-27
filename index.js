7eaa243d7d9ddebb4069bcfe1af0b9935edf744a
corentinghestem <61146780+corentinghestem@users.noreply.github.com>
2020-03-27 16:17:28 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,53 @@ const config = require("./storage/config.json");
 const fs = require("fs");
 bot.commands = new Discord.Collection();
 
+bot.on('guildMemberAdd', member => {
+    function date(date) {
+        return new Intl.DateTimeFormat('en-GB').format(date)
+    }
+    const randomWelcomeARR = [`Bienvenue à ${member.displayName}, amuse toi bien !`,
+     `${member.displayName} a rejoint le serveur, cachez-vous !`,
+    `Attention ! ${member.displayName} vient de débarquer.`,
+    `Arrêtez-tout ! ${member.displayName} est arrivé.`,
+    `${member.displayName} est enfin là, nous t'attendions !`]
+    const randomWelcome = randomWelcomeARR[Math.floor(Math.random() * randomWelcomeARR.length)]
+    const myWelcomeChannel = member.guild.channels.get(config.welcomeChannel)
+    if(!myWelcomeChannel) return console.log('l16')
+    const welcomEmbed = new Discord.RichEmbed()
+      .setColor('#5cf011')
+      .setTitle('**Un nouveau membre a rejoint**')
+      .setThumbnail(member.user.displayAvatarURL)
+      .setDescription(`${randomWelcome}
+      
+Nous sommes désormais ${member.guild.memberCount} membres dans le serveur.`)
+      .setFooter(`Tag : ${member.user.tag} | ID : ${member.user.id} | Créé le : ${date(member.user.createdAt)}`)
+    myWelcomeChannel.send(welcomEmbed)
+})
+
+bot.on('guildMemberRemove', member => {
+    function date(date) {
+        return new Intl.DateTimeFormat('en-GB').format(date)
+    }
+    const randomLeaveARR = [`${member.displayName} a terminé sa valise, à bientôt !`,
+`${member.displayName} a fait son choix, bon voyage !`,
+`Vers l'infini et au delà ! Bonne continuation ${member.displayName}.`,
+`Après des heures de réflexion, ${member.displayName} a décidé ... de 
+nous quitter !`,
+`${member.displayName} est partis ! Après tout un de perdu, dix de retrouvé. `]
+    const randomLeave = randomLeaveARR[Math.floor(Math.random() * randomLeaveARR.length)]
+    const myLeaveChannel = member.guild.channels.get(config.leaveChannel)
+    if(!myLeaveChannel) return console.log('l35')
+    const leavEmbed = new Discord.RichEmbed()
+      .setColor('#ff0000')
+      .setTitle('**Un membre a quitté le serveur**')
+      .setThumbnail(member.user.displayAvatarURL)
+      .setDescription(`${randomLeave}
+      
+Nous sommes désormais ${member.guild.memberCount} membres dans le serveur.`)
+      .setFooter(`Tag : ${member.user.tag} | ID : ${member.user.id} | Créé le : ${date(member.user.createdAt)}`)
+    myLeaveChannel.send(leavEmbed)
+})
+
 //LOGS DE SERVEURS
 bot.on('guildCreate', guild => {
     const formatDate = function(date) {
@@ -73,4 +120,3 @@ fs.readdir('./Events/', (error, f) => {
         });
 });
 bot.login(config.token);
-
